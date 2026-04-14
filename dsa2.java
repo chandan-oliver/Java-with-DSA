5dbbfbb91315266ebc64ac95fb59e5b7b9bf0576
chandan <ck42377@gmail.com>
2026-04-14 09:47:47 +0530

    Conditional Question

---
+++ dsa2.java
@@ -0,0 +1,15 @@
+package java_with_dsa;
+
+public class dsa2 {
+    public static void main(String args[]){
+        int a=10;
+        int b=20;
+        if(a>b){
+            System.out.println("a is largest than b");
+        }
+        else{
+            System.out.println("b is largest than a");
+        }
+    }
+    
+}
