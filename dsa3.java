b40b2e04f350cf8b827856cc82a6531f11718570
chandan <ck42377@gmail.com>
2026-04-14 21:26:55 +0530

    Conditional Question

---
+++ dsa3.java
@@ -0,0 +1,15 @@
+package java_with_dsa;
+import java.util.*;
+public class dsa3 {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        if(num%2==0){
+            System.out.println(num + " is even");
+        }
+        else{
+            System.out.println( num + " is odd");
+        }
+    }
+    
+}
