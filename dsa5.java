0fd625aa14444e8707c97c23ccdc6e7f709aa933
chandan <ck42377@gmail.com>
2026-04-15 07:25:16 +0530

    Conditional Question

---
+++ dsa5.java
@@ -0,0 +1,20 @@
+package java_with_dsa;
+
+public class dsa5 {
+    public static void main(String args[]){
+        int a=20;
+        int b=30;
+        int c=4;
+
+        if(a>=b & a>=c){
+            System.out.println("a is greater than b & c");
+        }
+        else if(b>=c){
+            System.out.println("b is greater than a & c");
+        }
+        else{
+            System.out.println("c is greater than a & b");
+        }
+    }
+    
+}
