93f8a469f950b0af3af3b2199c428b25f3838b75
chandan <ck42377@gmail.com>
2026-04-15 07:31:44 +0530

    Loop Question

---
+++ dsa6.java
@@ -0,0 +1,15 @@
+package java_with_dsa;
+
+public class dsa6 {
+    public static void main(String args[]){
+        int i=1;
+        while(i<=100){
+            System.out.println(i +  "-->" +  "hello world");
+            i++;
+        }
+        System.out.println(" you have print 100 times hello world");
+
+    }
+    
+    
+}
