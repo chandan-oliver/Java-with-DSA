7a362e864043f64476f91f4de08baac0422d8f92
chandan <ck42377@gmail.com>
2026-04-15 07:42:28 +0530

    Loop Question

---
+++ dsa7.java
@@ -0,0 +1,12 @@
+package java_with_dsa;
+// print num from 1-10
+public class dsa7 {
+    public static void main(String args[]){
+        int num=1;
+        while(num<=10){
+            System.out.println(num);
+            num++;
+        }
+    }
+    
+}
