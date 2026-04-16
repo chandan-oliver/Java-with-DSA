54d026f8ae2bd4ec9118982a4a0c182de48216c0
chandan <ck42377@gmail.com>
2026-04-16 06:31:51 +0530

    loop Question

---
+++ dsa13.java
@@ -0,0 +1,13 @@
+package java_with_dsa;
+// continue statement
+public class dsa13 {
+    public static void main(String args[]){
+        for(int i=1;i<=5;i++){
+            if(i==3){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+    
+}
