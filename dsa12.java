0b1c9269e46759a9964b126390f0750b0b9cff22
chandan <ck42377@gmail.com>
2026-04-16 06:28:04 +0530

    loop Question

---
+++ dsa12.java
@@ -0,0 +1,14 @@
+package java_with_dsa;
+// break statement
+public class dsa12 {
+    public static void main(String args[]){
+        for(int i=1;i<=5;i++){
+            if(i==3){
+                break;
+            }
+            System.out.println(i);
+            
+        }
+    }
+    
+}
