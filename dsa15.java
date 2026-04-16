1cf1a17af0693d44e775efbfa85a7777cc39ceb1
chandan <ck42377@gmail.com>
2026-04-16 06:49:17 +0530

    function Question

---
+++ dsa15.java
@@ -0,0 +1,16 @@
+package java_with_dsa;
+import java.util.*;
+// calculate sum
+public class dsa15 {
+    public static void calculatesum(){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int sum=a+b;
+        System.out.println("total sum of a + b is " + sum);
+    }
+    public static void main(String args[]){
+        calculatesum();
+    }
+    
+}
