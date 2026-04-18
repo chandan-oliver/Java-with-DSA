fc209756a80c7b2d3262d0c03f6bd37861595c00
chandan <ck42377@gmail.com>
2026-04-18 06:30:01 +0530

    function Question

---
+++ dsa24.java
@@ -0,0 +1,16 @@
+package java_with_dsa;
+// function over loading
+public class dsa24 {
+    public static int sum (int a,int b){
+        return a+b;
+    }
+    public static int sum(int a,int b,int c){
+        return a+b+c;
+    }
+    public static void main(String args[]){
+        System.out.println(sum(3,4));
+        System.out.println(sum(3,4,2));
+
+    }
+    
+}
